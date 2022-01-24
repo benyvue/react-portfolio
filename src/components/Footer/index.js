94374ca294bb9cf5758a8920c6609f87440a799b
MightyBenno <88509973+benyvue@users.noreply.github.com>
2022-01-24 17:20:07 -0600

    footer

---
+++ src/components/Footer/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Footer() {   
+    return (
+    <div className="page-footer" id="footer">
+        <a href="https://github.com/benyvue"><img src="https://img.icons8.com/ios/80/000000/github.png" alt="github-logo" class="icon"/></a>
+        <a href="https://www.linkedin.com/in/ben-vue-710205b6/"><img src="https://img.icons8.com/fluent/80/000000/linkedin-circled.png" alt="linkedin-logo" class="icon"/></a>
+        <a href="https://stackoverflow.com/users/16602953/ben-vue"><img src="https://img.icons8.com/ios/80/000000/stackoverflow.png" alt="stack-overflow" class="icon"/></a>
+    </div>
+    )
+}
+
+export default Footer;
